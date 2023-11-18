411cd38cbd589409ae1c4b109b4369e14a2c6354
duckmahn <21dh113296@st.huflit.edu.vn>
2023-11-18 14:50:35 +0700

    select-screen

---
+++ Assets/new game/Potrait Info.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PotraitInfo : MonoBehaviour
+{
+
+    public int posX;
+    public int posY;
+    public string characterId;
+    public Image img;
+}
